f21648a29d3ba0e4053dfe057b8457d370675a89
nbauma109 <nbauma109@github.com>
2025-11-30 09:12:36 +0100

    refactoring cleanup action test

---
+++ sample-project/src/main/java/demo/DemoApp.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class DemoApp {
+    static public void main(String[] args) {
+        System.out.println("Hello  " + " world");
+        if (true == Boolean.TRUE) {
+            System.out.println("messy");
+        }
+    }
+}
