4f1d94086c3838cfdfcca791fad4be08c5ee9395
nbauma109 <9403560+nbauma109@users.noreply.github.com>
2025-11-30 17:18:30 +0000

    Apply Eclipse cleanup refactoring

--- sample-project/src/main/java/demo/MessyImports.java
+++ sample-project/src/main/java/demo/MessyImports.java
@@ -2,13 +2,10 @@ package demo;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Map; // unused
-import java.io.File; // unused
 
 public class MessyImports {
 
-    public List<String> test() {
-        ArrayList<String> list = new ArrayList<>();
-        return list;
-    }
+	public List<String> test() {
+		return new ArrayList<String>();
+	}
 }
